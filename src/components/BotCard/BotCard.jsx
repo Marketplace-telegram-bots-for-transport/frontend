b53b41fac69b3bc705491f86a168f7406853a818
vladislav lis <lis.vladislav@gmail.com>
2023-09-26 17:01:18 +0400

    feat: add main page template

---
+++ src/components/BotCard/BotCard.jsx
@@ -0,0 +1,12 @@
+import styles from './BotCard.module.scss';
+
+const BotCard = ({ name, special }) => {
+	return (
+		<div className={styles.bot}>
+			<p>{name}</p>
+			<p>{special}</p>
+		</div>
+	);
+};
+
+export default BotCard;
