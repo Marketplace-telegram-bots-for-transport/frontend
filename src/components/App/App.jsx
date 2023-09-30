4f0bd862823bc85c73743d250a24bcc0633b52e6
Elizabeth Lis <elislis7@ya.ru>
2023-09-30 15:09:49 +0300

    feat(func open-close submenu): func open-close submenu, added styles for header
    func open-close submenu, added styles for header, change name image

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 
 const App = () => {
-  const [isLoggedIn /* , setIsLoggedIn */] = useState(false);
+  const navigate = useNavigate();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  /* Функция для выхода из профиля, 
+  должна будет стирать данные токена */
+  const handleLogOut = () => {
+    setIsLoggedIn(false);
+    navigate('/');
+  };
 
   return (
     <>
@@ -18,6 +27,7 @@ const App = () => {
               <Header
                 counterBots={/* bots.length */ 0}
                 isLoggedIn={isLoggedIn}
+                isLogOut={handleLogOut}
               />
               <Main />
               <Footer />
