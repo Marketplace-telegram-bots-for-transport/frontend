72ead23a455023949af6007777df8a9adbfb32a9
Elizabeth Lis <elislis7@ya.ru>
2023-10-06 21:07:15 +0300

    refactor(adjusting styles): creating variable styles
    creating variable styles and adjusting styles comp Header, Footer and common

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,6 @@ import styles from './Header.module.scss';
 
 import Submenu from './Submenu/Submenu';
 
-import search from '../../images/search-min.svg';
-
 function Header(props) {
   const { isLoggedIn, isLogOut } = props;
 
@@ -13,26 +11,15 @@ function Header(props) {
       <div className={styles.header__container}>
         <div className={styles.header__logoContainer}>
           <Link className={styles.header__logoLink} to='/' />
-          <h1 className={styles.header__logoTitle}>BotDepot</h1>
+          <h2 className={styles.header__logoTitle}>BotDepot</h2>
         </div>
         <div className={styles.header__search}>
-          <form
-            className={styles.header__searchForm}
-            noValidate
-            /* action='' */
-          >
-            <div className={styles.header__searchField}>
-              <img
-                className={styles.header__searchFieldImage}
-                src={search}
-                alt='Знак поиска'
-              />
-              <input
-                className={styles.header__searchFieldInput}
-                type='text'
-                placeholder='Текст'
-              />
-            </div>
+          <form className={styles.header__searchForm} noValidate>
+            <input
+              className={styles.header__searchFieldInput}
+              type='text'
+              placeholder='Текст'
+            />
           </form>
         </div>
         <Submenu isLoggedIn={isLoggedIn} counterBots={7} isLogOut={isLogOut} />
