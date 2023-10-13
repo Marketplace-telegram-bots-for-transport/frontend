2abf68fe0dd1a96d2aefac437b62f07bb4b43583
Elena Temirbekova <temn.lena@mail.ru>
2023-10-13 17:12:59 +0600

    feat(cart): add return to previous page

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -1,9 +1,13 @@
 import styles from './BackButton.module.scss';
 
 function BackButton() {
+  const handleGoBack = () => {
+    window.history.back();
+  };
+
   return (
     <div className={styles.returnElement}>
-      <button className={styles.returnElement__btn}>
+      <button className={styles.returnElement__btn} onClick={handleGoBack}>
         <p className={styles.returnElement__title}>Назад</p>
       </button>
       <p className={styles.returnElement__ref}>Главная страница / Корзина</p>
