f536416c9ca99431f29ec7cb07c8bb3c8a15be38
vladislav lis <lis.vladislav@gmail.com>
2023-11-11 13:14:30 +0400

    feat: special carousel images and reroutes

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-to-interactive-role */
 /* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Carousel from 'react-multi-carousel';
 import styles from './Banner.module.scss';
 import 'react-multi-carousel/lib/styles.css';
@@ -30,6 +31,7 @@ const CustomDot = ({ index, onClick, active }) => {
 };
 
 const Banner = () => {
+  const navigate = useNavigate();
   const isMobile = useWindowSize();
   const carouselRef = useRef(null);
 
@@ -140,9 +142,27 @@ const Banner = () => {
           customDot={<CustomDot />}
         >
           <div className={styles.banner__titlebox} />
-          <div className={styles.banner__titlebox} />
-          <div className={styles.banner__titlebox} />
-          <div className={styles.banner__titlebox} />
+          <div
+            className={`${styles.banner__titlebox} ${styles.banner__specialOne}`}
+            onClick={() => navigate('/special-offers/1')}
+            role='button'
+            tabIndex={0}
+            aria-label='special-1'
+          />
+          <div
+            className={`${styles.banner__titlebox} ${styles.banner__specialTwo}`}
+            onClick={() => navigate('/special-offers/2')}
+            role='button'
+            tabIndex={0}
+            aria-label='special-2'
+          />
+          <div
+            className={`${styles.banner__titlebox} ${styles.banner__specialThree}`}
+            onClick={() => navigate('/special-offers/3')}
+            role='button'
+            tabIndex={0}
+            aria-label='special-3'
+          />
         </Carousel>
       )}
     </div>
